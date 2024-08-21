61354278f1113efad71027f6b07abba63b642721
Junn-era <junnera@gmail.com>
2024-08-21 12:15:08 -0300

    Components Categories updated

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -2,21 +2,45 @@ import styles from './Categories.module.css';
 
 import Image from 'next/image';
 
-
 import icon1 from '../../../public/assets/entrada.png';
-// import icon2 from '../../../public/assets/img/massa.png';
-// import icon3 from '../../../public/assets/img/carne.png';
-// import icon4 from '../../../public/assets/img/bebidas.png';
-// import icon5 from '../../../public/assets/img/salada.png';
-// import icon6 from '../../../public/assets/img/sobremesa.png';
+import icon2 from '../../../public/assets/massa.png';
+import icon3 from '../../../public/assets/carne.png';
+import icon4 from '../../../public/assets/bebidas.png';
+import icon5 from '../../../public/assets/salada.png';
+import icon6 from '../../../public/assets/sobremesa.png';
 
 const Categories = ({ handleFilter, buttonClicked }) => {
   return (
     <section className={styles.section_categories}>
-      <div>
+      <div className={styles.container_buttons}>
         <button className={buttonClicked === 'Entries' ? styles.turnOnBtn : styles.turnOffBtn}
           onClick={() => handleFilter('Entries')}>
           <Image className={styles.icon} src={icon1} alt='Ícone' />Entradas
+        </button>
+
+        <button className={buttonClicked === 'Pasta' ? styles.turnOnBtn : styles.turnOffBtn}
+          onClick={() => handleFilter('Pasta')}>
+          <Image className={styles.icon} src={icon2} alt='Ícone' />Massas
+        </button>
+
+        <button className={buttonClicked === 'Meats' ? styles.turnOnBtn : styles.turnOffBtn}
+          onClick={() => handleFilter('Meats')}>
+          <Image className={styles.icon} src={icon3} alt='Ícone' />Carnes
+        </button>
+
+        <button className={buttonClicked === 'Drinks' ? styles.turnOnBtn : styles.turnOffBtn}
+          onClick={() => handleFilter('Drinks')}>
+          <Image className={styles.icon} src={icon4} alt='Ícone' />Bebidas
+        </button>
+
+        <button className={buttonClicked === 'Salads' ? styles.turnOnBtn : styles.turnOffBtn}
+          onClick={() => handleFilter('Salads')}>
+          <Image className={styles.icon} src={icon5} alt='Ícone' />Saladas
+        </button>
+
+        <button className={buttonClicked === 'Desserts' ? styles.turnOnBtn : styles.turnOffBtn}
+          onClick={() => handleFilter('Desserts')}>
+          <Image className={styles.icon} src={icon6} alt='Ícone' />Sobremesas
         </button>
       </div>
     </section>
